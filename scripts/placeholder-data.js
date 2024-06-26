9344de8e4a795ee95676a93410eb6d0cb74f2e25
Adam <awim256@gmail.com>
2024-06-26 13:09:10 -0700

    - Added script to generate and seed postgres database - Added additional libs to package.json

---
+++ scripts/placeholder-data.js
@@ -0,0 +1,73 @@
+// This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
+// https://nextjs.org/learn/dashboard-app/fetching-data
+const users = [
+  {
+    id: '410544b2-4001-4271-9855-fec4b6a6442a',
+    name: '',
+    email: '',
+    password: '',
+  },
+];
+
+const applications = [
+  {
+    company_name: 'Hootsuite',
+    position: 'Senior Front-End Developer',
+    application_date: '2024-06-01',
+    status: 'applied',
+    notes: 'Follow up in two weeks',
+    follow_up_date: '2024-06-15',
+    location: 'Vancouver, BC',
+    work_type: 'hybrid',
+    application_link: 'https://hootsuite.com/careers/job-posting'
+  },
+  {
+    company_name: 'SAP',
+    position: 'Developer',
+    application_date: '2024-06-05',
+    status: 'interviewing',
+    notes: 'Prepare for technical interview',
+    follow_up_date: '2024-06-20',
+    location: 'Vancouver, BC',
+    work_type: 'onsite',
+    application_link: 'https://sap.com/careers/job-posting'
+  },
+  {
+    company_name: 'Lululemon',
+    position: 'Senior Engineer',
+    application_date: '2024-06-10',
+    status: 'applied',
+    notes: 'Sent follow-up email',
+    follow_up_date: '2024-06-24',
+    location: 'Vancouver, BC',
+    work_type: 'remote',
+    application_link: 'https://lululemon.com/careers/job-posting'
+  },
+  {
+    company_name: 'Mastercard',
+    position: 'Software Engineer II',
+    application_date: '2024-06-12',
+    status: 'offered',
+    notes: 'Accepted offer',
+    follow_up_date: null,
+    location: 'Vancouver, BC',
+    work_type: 'hybrid',
+    application_link: 'https://mastercard.com/careers/job-posting'
+  },
+  {
+    company_name: 'Cority',
+    position: 'Senior Front-End Developer',
+    application_date: '2024-06-15',
+    status: 'rejected',
+    notes: 'Received rejection email',
+    follow_up_date: null,
+    location: 'Toronto, ON',
+    work_type: 'remote',
+    application_link: 'https://cority.com/careers/job-posting'
+  }
+];
+
+module.exports = {
+  users,
+  applications
+};
---
+++ scripts/seed.js
@@ -0,0 +1,105 @@
+const {db} = require('@vercel/postgres');
+const {applications, users,} = require('./placeholder-data.js');
+const bcrypt = require('bcrypt');
+
+async function seedUsers(client) {
+    try {
+        await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+        // Create the "users" table if it doesn't exist
+        const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS users (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
+      );
+    `;
+
+        console.log(`Created "users" table`);
+
+        // Insert data into the "users" table
+        const insertedUsers = await Promise.all(
+            users.map(async (user) => {
+                const hashedPassword = await bcrypt.hash(user.password, 10);
+                return client.sql`
+        INSERT INTO users (id, name, email, password)
+        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+        ON CONFLICT (id) DO NOTHING;
+      `;
+            }),
+        );
+
+        console.log(`Seeded ${insertedUsers.length} users`);
+
+        return {
+            createTable,
+            users: insertedUsers,
+        };
+    } catch (error) {
+        console.error('Error seeding users:', error);
+        throw error;
+    }
+}
+
+async function seedApplications(client) {
+    try {
+        await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+
+        // Create the "Applications" table if it doesn't exist
+        const createTable = await client.sql`
+    CREATE TABLE IF NOT EXISTS applications (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    company_name VARCHAR(255) NOT NULL,
+    position VARCHAR(255) NOT NULL,
+    application_date DATE NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    notes TEXT,
+    follow_up_date DATE,
+    location VARCHAR(255) NOT NULL,
+    work_type VARCHAR(50) NOT NULL,  
+    application_link VARCHAR(500),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
+`;
+
+        console.log(`Created "Applications" table`);
+
+        // Insert data into the "Applications" table
+        const insertedApplications = await Promise.all(
+            applications.map(
+                (application) => client.sql`
+        INSERT INTO applications (company_name, position, application_date, status, notes, follow_up_date, location, work_type, application_link)
+        VALUES (${application.company_name}, ${application.position}, ${application.application_date}, ${application.status}, ${application.notes}, ${application.follow_up_date}, ${application.location}, ${application.work_type}, ${application.application_link})
+        ON CONFLICT (id) DO NOTHING;
+      `,
+            ),
+        );
+
+        console.log(`Seeded ${insertedApplications.length} applications`);
+
+        return {
+            createTable,
+            applications: insertedApplications,
+        };
+    } catch (error) {
+        console.error('Error seeding applications:', error);
+        throw error;
+    }
+}
+
+async function main() {
+    const client = await db.connect();
+
+    await seedUsers(client);
+    await seedApplications(client);
+
+    await client.end();
+}
+
+main().catch((err) => {
+    console.error(
+        'An error occurred while attempting to seed the database:',
+        err,
+    );
+});
