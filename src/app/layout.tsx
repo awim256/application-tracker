0e356def46e75e0367a37a2b7249af1466b893b6
Adam <awim256@gmail.com>
2024-06-26 13:53:33 -0700

    - installed and added nextUI to application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,28 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
 import "./globals.css";
+import {NextUIProvider} from "@nextui-org/react";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: {
-    template: "%s | Application Tracker",
-    default: "Application Tracker"
-  },
-  description: "Track your job applications with ease.",
+    title: {
+        template: "%s | Application Tracker",
+        default: "Application Tracker"
+    },
+    description: "Track your job applications with ease.",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <NextUIProvider>
+            <html lang="en">
+            <body className={inter.className}>{children}</body>
+            </html>
+        </NextUIProvider>
+    );
 }
